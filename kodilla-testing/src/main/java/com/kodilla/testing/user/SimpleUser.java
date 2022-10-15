b77beab872eaa38d91a1015b09599a630dfbbe05
Pawel Pyrzyk <ppyrzyk@gmail.com>
2022-10-15 19:24:55 +0200

    pawel

--- kodilla-testing/src/main/java/com/kodilla/testing/user/SimpleUser.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/user/SimpleUser.java
@@ -2,12 +2,22 @@ package com.kodilla.testing.user;
     public class SimpleUser {
 
         final private String username;
+        private String realName;
 
+//        can be deleted??
         public SimpleUser(String username) {
             this.username = username;
+        }
+        public SimpleUser(String username, String realName) {
+            this.username = username;
+            this.realName = realName;
         }
 
         public String getUsername() {
             return username;
         }
-}
+        public String getRealName() {
+            return realName;
+        }
+    }
+
