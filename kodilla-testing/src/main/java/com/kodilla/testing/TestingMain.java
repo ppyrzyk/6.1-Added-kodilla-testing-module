1bcc8b401ce35ae97276153ef197b257da4315e6
Pawel Pyrzyk <ppyrzyk@gmail.com>
2022-10-15 18:42:24 +0200

    pawel

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -5,33 +5,6 @@ import com.kodilla.testing.user.SimpleUser;
 
 public class TestingMain {
     public static void main(String[] args) {
-        SimpleUser simpleUser = new SimpleUser("theForumUser");
-
-        String result1 = simpleUser.getUsername();
-
-            if (result1.equals("theForumUser")) {
-            System.out.println("test OK");
-            } else {
-            System.out.println("Error!");
-            }
-            System.out.println("Test - mój pierwszy test jednostkowy:");
-
-        Calculator calculator = new Calculator(2,4);
-
-            int result2 = calculator.getMultiplyResult();
-            if(result2 == 8) {
-                System.out.println("Multiplication has been completed successfully");
-            }else{
-                System.out.println("Error");
-            }
-            int result3 = calculator.getAddingResult();
-            if(result3 == 6 ){
-                System.out.println("Addition has been completed successfully");
-            }else{
-                System.out.println("Error");
-            }
-
 
     }
-
 }
--- kodilla-testing/src/test/java/com/kodilla/testing/forum/ForumTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/forum/ForumTestSuite.java
@@ -1,2 +1,27 @@
-package com.kodilla.testing.forum;public class ForumTestSuite {
+package com.kodilla.testing.forum;
+
+import com.kodilla.testing.user.SimpleUser;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("Forum Test Suite")
+class ForumTestSuite {
+
+    @DisplayName("When created SimpleUser with name, " +
+            "then getUsername should return correct name")
+
+    @Test
+    void testCaseUsername() {
+        //Given
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        //When
+        String result = simpleUser.getUsername();
+
+        String expectedResult = "theForumUser";
+
+        //Then
+        Assertions.assertEquals(expectedResult, result);
+    }
 }
