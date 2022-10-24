db49d057271fe58489118605619558eb63ae5a61
Pawel Pyrzyk <ppyrzyk@gmail.com>
2022-10-24 19:57:26 +0200

    pawel

--- kodilla-stream/src/main/java/com/kodilla/stream/immutable/ForumUser.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/immutable/ForumUser.java
@@ -5,16 +5,15 @@ public class ForumUser {
     private final String username;
     private final String realName;
 
-    public ForumUser(final String username,final String realName) {
+    public ForumUser(final  String username, final String realName) {
         this.username = username;
         this.realName = realName;
     }
 
     public String getUsername() {
         return username;
-    }
-
-    public String getRealName() {
+    }  public String getrRealName() {
         return realName;
     }
 }
+
