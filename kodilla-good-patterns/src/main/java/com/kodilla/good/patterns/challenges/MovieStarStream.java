9ccd387845cead8e742feb7aa28871741222d6b0
Pawel Pyrzyk <ppyrzyk@gmail.com>
2022-12-01 21:57:54 +0100

    Module 13.1

---
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/MovieStarStream.java
@@ -0,0 +1,18 @@
+package com.kodilla.good.patterns.challenges;
+
+import java.util.stream.Collectors;
+
+public class MovieStarStream {
+
+    public static void main( String[] args) {
+
+        MovieStore MS = new MovieStore();
+
+        String titlesList = MS.getMovies().entrySet().stream()
+                .flatMap(m->m.getValue().stream())
+                .collect(Collectors.joining("! "));
+
+        System.out.println(titlesList);
+    }
+
+}
