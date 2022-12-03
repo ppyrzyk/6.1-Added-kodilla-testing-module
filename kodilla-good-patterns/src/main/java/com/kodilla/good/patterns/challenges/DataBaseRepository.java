8505762c983841145439bcfb1471992c8e5dd4f2
Pawel Pyrzyk <ppyrzyk@gmail.com>
2022-12-03 15:55:53 +0100

    Module 13.2

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/DataBaseRepository.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/DataBaseRepository.java
@@ -1,12 +1,13 @@
 package com.kodilla.good.patterns.challenges;
 
 import java.time.LocalDate;
+import java.util.Locale;
 
 public class DataBaseRepository implements SaleRepository{
 
     @Override
     public void createSoldProduct(User user, Product product, LocalDate saleDate) {
-        System.out.println(product.getNameOfProduct() + " has been sold on " + saleDate + " ,purchased by " + user.getLogin() +
-                "\n Contact the user via e-mail at " + user.getEmail()+ " .");
+        System.out.println(product.getNameOfProduct().toUpperCase(Locale.ROOT) + " has been sold on " + saleDate + " ,purchased by " + user.getLogin());
+        System.out.println("\n Contact the user via e-mail at " + user.getEmail()+ " .");
     }
 }
